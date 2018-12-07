1df48d2d978bbb690c0a903f336ae8ad2912e315
cvillarreal1997 <terrycarlos@live.com>
2018-12-07 17:35:40 -0500

    incomen anual

--- TaxHomework/src/ec/edu/espe/model/Tax_Calculation.java
+++ TaxHomework/src/ec/edu/espe/model/Tax_Calculation.java
@@ -6,13 +6,74 @@
 package ec.edu.espe.model;
 
 import ec.edu.espe.util.Operation;
+import static ec.edu.espe.util.Operation.multi;
+import static ec.edu.espe.util.Operation.sub;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author JDIsmael
  */
 public class Tax_Calculation {
+    
+    Person person = new Person();
+    
+    int sri=convertToCents(9.45f);
+    String id;
+    float livingplace,education,clothing,health,salary;
+    Scanner in= new Scanner (System.in);
+    
+    
+    
+    
+    public void indatas(){
+        
+        System.out.println("Ingrese cedula o ruc");
+        person.setId(in.nextLine());
+        System.out.println("Ingrese Sueldo");
+        person.setSalary(in.nextFloat());        
+        System.out.println("              Ingrese Gatos");
+        System.out.println("Vivienda:");
+        person.setLivingplace(in.nextFloat());
+        System.out.println("Educacion:");
+        person.setEducation(in.nextFloat());
+        System.out.println("Vestimenta:");
+        person.setClothing(in.nextFloat());
+        System.out.println("Salud:");
+        person.setHealth(in.nextFloat());
+   
+        
+    }
+    public int inmonthneto(){
+        int salaryint=convertToCents(person.getSalary());
+        int srit;
+        srit = ((sri*salaryint)/10000);
+        int monthneto;
+        monthneto=sub(salaryint,srit);
+        return monthneto;
+    }
+    public int incomeage(){
+        int valoranual=multi(inmonthneto(),12);
+    return valoranual;
+    }
+    public int deductiblExpenses(){
+        int deductible;
+        deductible=convertToCents(person.getHealth()+person.getLivingplace()+person.getEducation()+person.getClothing());
+        if(deductible>incomeage()) {
+            System.out.println("no puede gastas mas de lo que gana");
+            System.out.println("su sueldo es solo de"+inmonthneto());
+            return -1;
+        }
+       
+        return deductible;
+    }
+    
+    
+    
+    
+    
+    
     
     private ArrayList<Integer> limitFractionTax;
     private ArrayList<Integer> basicFractionTax;
