0b6e53e190d0a231cecb9590ff21e2c6c1be1b66
cvillarreal1997 <terrycarlos@live.com>
2018-12-07 13:25:36 -0500

    person data

--- TaxHomework/src/ec/edu/espe/model/Person.java
+++ TaxHomework/src/ec/edu/espe/model/Person.java
@@ -5,10 +5,80 @@
  */
 package ec.edu.espe.model;
 
+import java.util.Scanner;
+
 /**
  *
  * @author JDIsmael
  */
 public class Person {
+     private String id;
+    private float livingplace;
+    private float education;
+    private float clothing;
+    private float health;
+    private float salary;
+    Scanner in= new Scanner(System.in);
+    public Person(){
+        
+    }
+    
+    
+    
+    public void dateperson(){
+        System.out.println("Ingrese cedula o ruc");
+        id=in.nextLine();
+        System.out.println("Ingrese Sueldo");
+        salary=in.nextFloat();
+        
+        System.out.println("              Ingrese Gatos");
+        System.out.println("Vivienda:");
+        livingplace=in.nextFloat();
+        System.out.println("Educacion:");
+        education=in.nextFloat();
+        System.out.println("Vestimenta:");
+        clothing=in.nextFloat();
+        System.out.println("Salud:");
+        health=in.nextFloat();      
+        
+                
+    }   
+
+    public String getId() {
+        return id;
+    }
+
+    public float getLivingplace() {
+        return livingplace;
+    }
+
+    public float getEducation() {
+        return education;
+    }
+
+    public float getClothing() {
+        return clothing;
+    }
+
+    public float getHealth() {
+        return health;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public Scanner getIn() {
+        return in;
+    }
+
+    public Person(String id, float livingplace, float education, float clothing, float health, float salary) {
+        this.id = id;
+        this.livingplace = livingplace;
+        this.education = education;
+        this.clothing = clothing;
+        this.health = health;
+        this.salary = salary;
+    }
     
 }
